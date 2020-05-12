e15fe7f1f5fe5a6a9d79b67f366301d407dab1ab
Selda Güzel Steltner <selda.steltner@complevo.de>
2020-05-12 21:35:47 +0200

    sidebar and toolbar UI

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav-list.component.css']
 })
 export class SidenavListComponent implements OnInit {
+  @Output() sidenavToggle = new EventEmitter <void>();
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onToggleSidenav() {
+    this.sidenavToggle.emit()
   }
 
 }
